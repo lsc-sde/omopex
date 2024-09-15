fe4443bc8cf00cdbe04e5ef101f895527e9069fe
vvcb <vvcb.n1@gmail.com>
2024-09-15 01:38:38 +0100

    Remove old dbt macro

--- omopex/projects/p_abx/macros/macro_remove_pts.sql
+++
@@ -1,9 +0,0 @@
-{% macro remove_pts() %}
-
-DELETE FROM dbt_omop.gold.person
-WHERE person_id NOT IN (SELECT person_id FROM dbt_omop.gold.observation_period);
-
-DELETE FROM dbt_omop.gold.death
-WHERE person_id NOT IN (SELECT person_id FROM dbt_omop.gold.observation_period);
-
-{% endmacro %}
